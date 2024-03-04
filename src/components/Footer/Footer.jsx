ee157a568dc8ce55add569ea25c2ec3b7bee7132
murtazahasan <murtazahasan276@gmail.com>
2024-03-03 19:02:09 -0800

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-primary text-white text-center py-4">
+      <div>copyright &copy; 2024 || Orbit-Guide</div>
+    </div>
+  );
+};
+
+export default Footer;
